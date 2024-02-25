cfc19d0d1532727d24394d3317329fc0a1c63e08
Cesarribe <cesar.ribeiro_@hotmail.com>
2024-02-25 01:19:41 -0300

    commit

---
+++ src/test/java/br/com/cesaribeiro/SpringBootExemplo/SpringBootExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesaribeiro.SpringBootExemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
